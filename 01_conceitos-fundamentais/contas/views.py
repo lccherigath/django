442c04369c569772d2c5ff599a28ceb0f67148d2
Lucas Cherigath <lucas02.cherigath@gmail.com>
2018-07-26 23:43:46 -0300

    CRUD: Update & Delete

--- 01_conceitos-fundamentais/contas/views.py
+++ 01_conceitos-fundamentais/contas/views.py
@@ -23,10 +23,12 @@ def home(request):
     data['now'] = datetime.datetime.now()
     return render(request, 'contas/home.html', data)
 
+
 def listagem(request):
 	data = {}
 	data['transacoes'] = Transacao.objects.all()
 	return render(request, 'contas/listagem.html', data)
+
 
 def nova_transacao(request):
 	form = TransacaoForm(request.POST or None)
@@ -36,3 +38,23 @@ def nova_transacao(request):
 		return redirect('url_listagem')
 
 	return render(request, 'contas/form.html', {'form': form})
+
+
+def update(request, pk):
+	transacao = Transacao.objects.get(pk=pk)
+
+	form = TransacaoForm(request.POST or None, instance=transacao)
+
+	if form.is_valid():
+		form.save()
+		return redirect('url_listagem')
+
+	return render(request, 'contas/form.html', {'form': form, 'transacao': transacao})
+
+
+def delete(request, pk):
+	transacao = Transacao.objects.get(pk=pk)
+
+	transacao.delete()
+
+	return redirect('url_listagem')
